a8b45698b1a089ba9c44f22786263ba0a2be1a28
sudikulal <sudikulal325@gmail.com>
2024-06-23 15:50:58 +0530

    fix cors issue

--- API/main.go
+++ API/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"time"
 
-    "journal/routes"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
+	"journal/routes"
 )
 
 func main() {
     router := gin.Default()
+
+    router.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"*"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
 
     routes.UserRoutes(router)
     routes.JournalRoutes(router)
